9fe0c7a30055a690113b66bf21a6c98a93fac8ba
DeeMeyers <justdmeyers@gmail.com>
2020-02-04 16:22:24 -0500

    sample code and moment js added

--- code.js
+++ code.js
@@ -14,3 +14,34 @@
 // im going to use flex to put this info inside of a div per customer, using more 
 // divs for each bit of info. Should be able to use js to create all the elements. 
 
+
+
+const custumerNames = customers.map(function (customer){
+    return customer.name.first[0].toUpperCase() + customer.name.slice(1) + 
+    ' ' +
+    customer.name.last[0].toUpperCase() + 
+    customer.name.last.slice(1)
+})
+
+function capitalizeFirstLetter(string){
+    return string[0].toUpperCase + string.slice(1)
+}
+
+const custumerNames = customers.map(function (customer){
+    return capitalizeFirstLetter(customer.name.first) +
+    ' ' +
+    capitalizeFirstLetter(customer.name.last)
+})
+
+
+const customerListItems = customerNames.map(function (name){
+    const li = document.createElement('li')
+    li.textContent = name
+    return li
+})
+
+const ul = document.createElement('ul')
+for (const li of customerListItems){
+    ul.appendChild(li)
+}
+document.querySelector('#customers').appendChild(ul)
