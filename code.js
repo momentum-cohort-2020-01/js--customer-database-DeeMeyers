652c4483275e6861ccfd793c67f777dc644b4adc
DeeMeyers <justdmeyers@gmail.com>
2020-02-04 14:32:03 -0500

    took notes

---
+++ code.js
@@ -0,0 +1,16 @@
+//List of customers is const "customers"
+//  
+//for each customer you need to pull:
+    // thumbnail pic
+        //will need css styling
+    // email
+        //will need css styling
+    // adress
+        //will need to change full state name to abbreviation
+    // DOB
+        //will need to be changed with moment.js
+    // customer since: 
+        //will need to be calculated with moment.js
+// im going to use flex to put this info inside of a div per customer, using more 
+// divs for each bit of info. Should be able to use js to create all the elements. 
+
