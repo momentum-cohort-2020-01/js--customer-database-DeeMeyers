a3a8906bc1dad8af42159364103abb40f8bad3d6
DeeMeyers <justdmeyers@gmail.com>
2020-02-04 20:00:02 -0500

    gathered info into an array each. trying to make function that will make a div for each item in an array and assign them all the same class for later formatting

--- code.js
+++ code.js
@@ -1,13 +1,13 @@
 //List of customers is const "customers"
 //  
 //for each customer you need to pull:
-    // thumbnail pic
+    // thumbnail pic - done
         //will need css styling
-    // email
-        //will need css styling
-    // adress
+    // email - done
+        //will need css styling 
+    // adress - need to capitalize stuff, states done
         //will need to change full state name to abbreviation
-    // DOB
+    // DOB -done
         //will need to be changed with moment.js
     // customer since: 
         //will need to be calculated with moment.js
@@ -16,32 +16,79 @@
 
 
 
-const custumerNames = customers.map(function (customer){
-    return customer.name.first[0].toUpperCase() + customer.name.slice(1) + 
-    ' ' +
-    customer.name.last[0].toUpperCase() + 
-    customer.name.last.slice(1)
-})
 
 function capitalizeFirstLetter(string){
-    return string[0].toUpperCase + string.slice(1)
+    return string[0].toUpperCase() + string.slice(1)
 }
 
-const custumerNames = customers.map(function (customer){
+const customerNames = customers.map(function (customer){
     return capitalizeFirstLetter(customer.name.first) +
     ' ' +
     capitalizeFirstLetter(customer.name.last)
 })
 
+console.log(customerNames);
 
 const customerListItems = customerNames.map(function (name){
-    const li = document.createElement('li')
-    li.textContent = name
-    return li
+    const div = document.createElement('div')
+    div.textContent = name
+    return div
 })
-
+console.log(customerListItems);
+console.log(customerListItems[2]);
 const ul = document.createElement('ul')
+console.log(ul);
+
 for (const li of customerListItems){
     ul.appendChild(li)
 }
 document.querySelector('#customers').appendChild(ul)
+//gets me all the picture urls
+const customerPics = customers.map(function (customer){
+    return customer.picture.thumbnail
+})
+
+//insert pics
+
+console.log( customerPics)
+
+//gets me all the emails
+const customerEmails = customers.map(function (customer){
+    return customer.email
+})
+
+console.log(customerEmails)
+
+//get all adress and make them ok
+
+const states = customers.map(function(customer){
+    return nameToAbbr(customer.location.state)
+})
+console.log(states);
+
+const adressObj = customers.map(function (customer){
+    return customer.location.street + ' ' + customer.location.city
+})
+
+console.log(adressObj);
+
+//get DOB
+
+const dob = customers.map(function(customer){
+    return "DOB: "+moment(customer.dob).format('MMMM Do, YYYY')
+})
+console.log({dob})
+
+//crust since
+
+const custSince = customers.map(function(customer){
+    return "Customer Since: "+moment(customer.registered).format('MMMM Do, YYYY')
+})
+
+//make a div with a shared class for each item in an array
+console.log(custSince)
+ function makeDiv(arr, class){
+     for(let each of arr){
+
+     }
+ }
